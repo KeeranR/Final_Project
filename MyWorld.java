2aa071093236b61672502ea61ffa59bd2b4d15f1
Keeran <43045600+KeeranR@users.noreply.github.com>
2019-01-16 15:27:50 -0600

    update 1
    update 1.
    
    1/16/2019

---
+++ MyWorld.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends greenfoot.World
+{    
+    private GifImage gif;
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        keyPressed();
+    }
+    public void keyPressed()
+    {
+        if(Greenfoot.isKeyDown("space") == true)
+        {
+            GifImage gifImage = new GifImage("Yacht Yacht.gif");
+        }
+       
+    }
+}
